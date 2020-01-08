502386cbcc16d6771581b9d4e30cbf1f1009d8b4
nicopons24 <nicopons24@gmail.com>
2020-01-09 00:30:54 +0100

    Boton añadir evento Inicio ok

--- src/js/funcionBotones.js
+++ src/js/funcionBotones.js
@@ -1,15 +1,44 @@
-function btnInicioClick(){
-  $( "#btnInicio" ).bind( "click", function(event, ui) {
-    $.ajax({
-      url:'planing.html',
-      success:function(data){
-        $(".ui-content").html(data);
-      }
-    });
-  });
-}
+function btnInicioClick() {
+	$("#title").text(function(i, text) {
+		var today = new Date();
+		return today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+	});
+	$("#btnInicio").bind("click", function(event, ui) {
+		$.ajax({
+			url : 'planing.html',
+			success : function(data) {
+				$(".ui-content").html(data);
+			}
+		});
+	});
+};
 
-function init(){
-	console.log("onload");
-	btnInicioClick();
-}
+function btnNevoEventoClick(){
+	$("#title").text(function(i, text) {
+		return "Nuevo Evento";
+	});
+	$("#btnNuevoEvento").bind("click", function(event, ui) {
+		$.ajax({
+			url : 'nuevoEvento.html',
+			success : function(data) {
+				$(".ui-content").html(data);
+			}
+		});
+	});
+};
+
+function init() {
+	$(document).ready(function() {
+		$("#title").text(function(i, text) {
+		var today = new Date();
+		return today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+	});
+		$.ajax({
+			url : 'planing.html',
+			success : function(data) {
+				$(".ui-content").html(data);
+			}
+		});
+	});
+};
+
