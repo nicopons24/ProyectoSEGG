a48fecbbebc64355ed2515c89e5bb767d1e0732c
Nicolas <nicopons24@gmail.com>
2020-01-09 01:26:02 +0100

    Nuevo evento

--- src/js/funcionBotones.js
+++ src/js/funcionBotones.js
@@ -3,42 +3,28 @@ function btnInicioClick() {
 		var today = new Date();
 		return today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
 	});
-	$("#btnInicio").bind("click", function(event, ui) {
-		$.ajax({
-			url : 'planing.html',
-			success : function(data) {
-				$(".ui-content").html(data);
-			}
-		});
+
+	$.ajax({
+		url : 'planing.html',
+		success : function(data) {
+			$(".ui-content").html(data);
+		}
 	});
 };
 
-function btnNevoEventoClick(){
+function btnNevoEventoClick() {
 	$("#title").text(function(i, text) {
 		return "Nuevo Evento";
 	});
-	$("#btnNuevoEvento").bind("click", function(event, ui) {
-		$.ajax({
-			url : 'nuevoEvento.html',
-			success : function(data) {
-				$(".ui-content").html(data);
-			}
-		});
+	$.ajax({
+		url : 'nuevoEvento.html',
+		success : function(data) {
+			$(".ui-content").html(data);
+		}
 	});
 };
 
 function init() {
-	$(document).ready(function() {
-		$("#title").text(function(i, text) {
-		var today = new Date();
-		return today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
-	});
-		$.ajax({
-			url : 'planing.html',
-			success : function(data) {
-				$(".ui-content").html(data);
-			}
-		});
-	});
+	btnInicioClick();
 };
 
